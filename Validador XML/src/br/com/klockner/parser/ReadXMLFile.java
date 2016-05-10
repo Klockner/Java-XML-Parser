79ad00c154c02fb9a35608a638f95032d73dff36
Gabriel Klockner <gabriel.klockner@gmail.com>
2016-05-09 22:22:56 -0300

    Implemented until test case 3 (Classification, Instance verification and Consistency

--- Validador XML/src/br/com/klockner/parser/ReadXMLFile.java
+++ Validador XML/src/br/com/klockner/parser/ReadXMLFile.java
@@ -11,7 +11,6 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -23,59 +22,6 @@ import org.xml.sax.SAXException;
  * @author Gabriel
  */
 public class ReadXMLFile {
-    
-    public void parseXml(String xmlFileName) {
-        try {
-            File xmlFile = new File(xmlFileName);
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(xmlFile);
-            
-            //optional, but recommended
-            //http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
-            doc.getDocumentElement().normalize();
-
-            System.out.println("Elemento raiz: " + doc.getDocumentElement().getNodeName());
-            
-            NodeList nList = doc.getElementsByTagName("ProdutoDeModa");
-            
-            System.out.println("\n----------------------------");
-            
-            for (int i = 0; i < nList.getLength(); i++) {
-                Node nNode = nList.item(i);
-                System.out.println("\nElemento: " + nNode.getNodeName());
-                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                    Element element = (Element) nNode;
-                    
-                    System.out.println("SKU: " + element.getElementsByTagName("SKU").item(0).getTextContent());
-                    System.out.println("Nome: " + element.getElementsByTagName("Nome").item(0).getTextContent());
-                    System.out.println("Descrição: " + element.getElementsByTagName("Descricao").item(0).getTextContent());
-                    System.out.println("Marca: " + element.getElementsByTagName("Marca").item(0).getTextContent());
-                    System.out.println("Loja: " + element.getElementsByTagName("Loja").item(0).getTextContent());
-                    System.out.println("Preco: " + element.getElementsByTagName("Preco").item(0).getTextContent());
-                    System.out.println("Preco Promoção: " + element.getElementsByTagName("PrecoPromocao").item(0).getTextContent());
-                    System.out.println("Número de parcelas: " + element.getElementsByTagName("NumeroDeParcelas").item(0).getTextContent());
-                    System.out.println("Valor das parcelas: " + element.getElementsByTagName("ValorDaParcela").item(0).getTextContent());
-                    System.out.println("URL: " + element.getElementsByTagName("URL").item(0).getTextContent());
-                    System.out.println("URL da imagem: " + element.getElementsByTagName("URLImagem").item(0).getTextContent());
-                    System.out.println("Preenchimento: " + element.getElementsByTagName("Preenchimento").item(0).getTextContent());
-                    System.out.println("Cor: " + element.getElementsByTagName("Cor").item(0).getTextContent());
-                    System.out.println("Textura: " + element.getElementsByTagName("Textura").item(0).getTextContent());
-                    System.out.println("Tamanho: " + element.getElementsByTagName("Tamanho").item(0).getTextContent());
-                    System.out.println("Classificação: " + element.getElementsByTagName("Classificacao").item(0).getTextContent());
-                    System.out.println("Categoria: " + element.getElementsByTagName("Categoria").item(0).getTextContent());
-                    System.out.println("Subcategoria: " + element.getElementsByTagName("Subcategoria").item(0).getTextContent());
-                }
-            }
-            
-        } catch (IOException e) {
-            System.out.println("Erro ao abrir o arquivo: " + e.getMessage());
-        } catch (ParserConfigurationException e) {
-            System.out.println("Erro ao parsear o arquivo: " + e.getMessage());
-        } catch (SAXException e) {
-            System.out.println("Erro no SAX: " + e.getMessage());
-        }
-    }
     
     /**
      * 
---
+++ Validador XML/src/br/com/klockner/provas/Prova3.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.klockner.provas;
+
+import br.com.klockner.util.OpenFile;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+/**
+ *
+ * @author klockner
+ */
+public class Prova3 {
+    private final OpenFile openFile;
+    private final Document doc;
+    private String tamanho;
+    private final String tituloProva;
+    
+    public Prova3 (String xmlFileName, String tituloProva) {
+        openFile = new OpenFile();
+        doc = openFile.createDoc(xmlFileName);
+        this.tituloProva = tituloProva;
+    }
+    
+    public void initParse() {
+        System.out.println(tituloProva);
+        System.out.println("-----------------------------");
+        
+        NodeList nodeList = doc.getElementsByTagName("ProdutoDeModa");
+        boolean isPlusSize = false;
+        
+        //Percorre os nodes da árvore de "ProdutoDeModa"
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node nNode = nodeList.item(i);
+            
+            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element element = (Element) nNode;
+                if (element.getElementsByTagName("Categoria").item(0).getTextContent().equalsIgnoreCase("calça")) {
+                    
+                    for (int j = 0; j < element.getElementsByTagName("Tamanho").getLength(); j++) {
+                        isPlusSize = false;
+                        tamanho = element.getElementsByTagName("Tamanho").item(j).getTextContent();
+
+                        if (tamanho.matches("G|GG|GGG|L|XL|XXL")) {
+                            isPlusSize = true;
+                        }
+                    }
+                    if (isPlusSize) {
+                        System.out.println("Elemento: " + nNode.getNodeName());
+                        System.out.println("SKU: " + element.getElementsByTagName("SKU").item(0).getTextContent());
+                        System.out.println("Nome: " + element.getElementsByTagName("Nome").item(0).getTextContent());
+                        System.out.println("Categoria: " + element.getElementsByTagName("Categoria").item(0).getTextContent());
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+}
